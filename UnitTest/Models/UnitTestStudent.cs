9768a5206a648e1b5bea8651847dd409d4b1b8f8
zxjte9411 <a5852241@gmail.com>
2020-05-06 04:17:21 +0800

    改測試

--- UnitTest/Models/UnitTestStudent.cs
+++ UnitTest/Models/UnitTestStudent.cs
@@ -14,17 +14,22 @@ namespace GradeManageSystem.Models.Tests
         {
             UserInformation info = new UserInformation("test", "0912345678", "AAAA", new DateTime(2000, 7, 15), "男");
             Course course = new Course("123456789", "math", 105, 1);
-            Grade grade = new Grade(100);
-            Dictionary<Course, Grade> courseGrades = new Dictionary<Course, Grade>();
+            List<Course> courses = new List<Course>();
+            courses.Add(course);
+            int grade = 100;
+            Dictionary<Course, int> courseGrades = new Dictionary<Course, int>();
             courseGrades.Add(course, grade);
-
+            Dictionary<string, int> courseGrade = new Dictionary<string, int>();
+            courseGrade.Add(course.Id, grade);
             Student student = new Student("102580039", "123456789", 3, info, courseGrades);
 
             Assert.AreEqual("102580039", student.Id);
             Assert.AreEqual("123456789", student.Password);
             Assert.AreEqual(3, student.Authority);
             Assert.AreEqual(info, student.UserInformation);
-            Assert.AreEqual(courseGrades, student.CourseGrades);
+            Assert.AreEqual(courseGrade[course.Id], student.CourseGrades[course.Id]);
+            Assert.AreEqual(courses[0].Id, student.Courses[0].Id);
+            Assert.AreEqual(courses[0].Name, student.Courses[0].Name);
         }
     }
 }
