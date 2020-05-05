159c5eec39508e7aa949c07a1a2544ec51479e94
zxjte9411 <a5852241@gmail.com>
2020-05-06 07:25:56 +0800

    解決介面無法序列化成 JSON 的問題

---
+++ GradeManageSystem/Models/AccountModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GradeManageSystem.Models
+{
+    public class AccountModel : IAccount
+    {
+        public string Id { get; set; }
+        public string Password { get; set; }
+        public int Authority { get; set; }
+        public UserInformation UserInformation { get; set; }
+    }
+}
