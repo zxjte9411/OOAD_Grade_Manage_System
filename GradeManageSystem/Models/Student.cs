93e82dc6ebe3e0d5b882ae7b42db201b40c2ef13
zxjte9411 <a5852241@gmail.com>
2020-05-06 03:52:20 +0800

    更改儲存成績的方式

--- GradeManageSystem/Models/Student.cs
+++ GradeManageSystem/Models/Student.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,19 +8,26 @@ namespace GradeManageSystem.Models
     public class Student : IAccount
     {
         public Student(string id, string password, int authority,
-            UserInformation userInformation, Dictionary<Course, Grade> CourseGrade)
+            UserInformation userInformation, Dictionary<Course, int> courseGrade)
         {
             Id = id;
             Password = password;
             Authority = authority;
             UserInformation = userInformation;
-            CourseGrades = CourseGrade;
+            Courses = new List<Course>();
+            CourseGrades = new Dictionary<string, int>();
+            foreach (var course in courseGrade.Keys)
+            {
+                Courses.Add(course);
+                CourseGrades.Add(course.Id, courseGrade[course]);
+            }
         }
         public string Id { get; set; }
         public string Password { get; set; }
         public int Authority { get; set; }
         public UserInformation UserInformation { get; set; }
-        public Dictionary<Course, Grade> CourseGrades { get; set; }
+        public List<Course> Courses { get; set; }
+        public Dictionary<string, int> CourseGrades { get; set; }
         
     }
 }
