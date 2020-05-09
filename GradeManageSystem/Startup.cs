975bd6d2b956f2a717d544e511021cb245706dc0
zxjte9411 <a5852241@gmail.com>
2020-05-10 00:52:56 +0800

    設定驗證機制及安裝所需套件
    這邊即將新增登入通能

--- GradeManageSystem/Startup.cs
+++ GradeManageSystem/Startup.cs
@@ -11,6 +11,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using GradeManageSystem.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace GradeManageSystem
 {
@@ -31,6 +35,30 @@ namespace GradeManageSystem
                 options.AddPolicy("AllowMyOrigin",
                 builder => builder.AllowAnyHeader().AllowAnyOrigin());
             });
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => 
+            {
+                // 當驗證失敗時，回應標頭會包含 WWW-Authenticate 標頭，這裡會顯示失敗的詳細錯誤原因
+                options.IncludeErrorDetails = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    // 透過這項宣告，就可以從 "sub" 取值並設定給 User.Identity.Name
+                    NameClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier",
+                    // 透過這項宣告，就可以從 "roles" 取值，並可讓 [Authorize] 判斷角色
+                    RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role",
+                    // 一般都會驗證 Issuer
+                    ValidateIssuer = true,
+                    ValidIssuer = Configuration.GetValue<string>("JwtSettings:Issuer"),
+
+                    // 一般不太需要驗證 Audience
+                    ValidateAudience = false,
+                    // 一般都會驗證 Token 的有效期間
+                    ValidateLifetime = true,
+                    // 如果 Token 中包含 key 才需要驗證，一般都只有簽章而已
+                    ValidateIssuerSigningKey = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetValue<string>("JwtSettings:SignKey")))
+                };
+            });
+
             services.AddSingleton<DomainController>();
         }
 
@@ -48,7 +76,9 @@ namespace GradeManageSystem
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseAuthentication();// 先驗證
+
+            app.UseAuthorization();// 再授權
 
             app.UseEndpoints(endpoints =>
             {
