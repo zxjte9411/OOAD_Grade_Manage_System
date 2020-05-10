99c0a38e66e3339d33e21dbaf645c088ab996695
zxjte9411 <a5852241@gmail.com>
2020-05-10 12:59:56 +0800

    modify style

--- gms_frontend/src/App.vue
+++ gms_frontend/src/App.vue
@@ -7,23 +7,21 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Noto Sans TC', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
