5b44fe5ee68722a71581f71f9214dd2eade84e79
zxjte9411 <a5852241@gmail.com>
2020-05-15 01:31:42 +0800

    首頁變成 GradeManage

--- gms_frontend/src/router.js
+++ gms_frontend/src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './components/HelloWorld.vue'
+// import Home from './components/HelloWorld.vue'
 
 
 Vue.use(Router)
@@ -10,7 +10,7 @@ export default new Router({
   routes: [{
     path: '/',
     name: 'home',
-    component: Home
+    component: () => import('./views/GradeManage.vue')
   },
   {
     path: '/grademanage',
